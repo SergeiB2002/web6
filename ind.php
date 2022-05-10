5be658cac0086bf691db5630c3c94694c3fcb89b
Sergei Brezitskii <90458734+SergeiB2002@users.noreply.github.com>
2022-05-10 18:24:22 +0300

    Update ind.php

--- ind.php
+++ ind.php
@@ -133,4 +133,175 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   }
   include('form.php');
 }
+else {
+  if(!empty($_POST['edit'])){
+    $id=$_POST['dd'];
+    $name = $_POST['field-name'];
+    $email = $_POST['field-email'];
+    $year = $_POST['year'];
+    $pol=$_POST['radio-pol'];
+    $limbs=$_POST['radio-limb'];
+    $powers=$_POST['field-super'];
+    $bio=$_POST['field-bio'];
+    
+    $errors = FALSE;
+    
+    if (empty($name) || (!preg_match($nameregex,$name))) {
+      setcookie('name_error', '1', time() + 24*60 * 60);
+      setcookie('name_value', '', 100000);
+      $errors = TRUE;
+    }
+    else {
+      setcookie('name_value', $fio, time() + 60 * 60);
+      setcookie('name_error','',100000);
+    }
 
+    if (empty($email) || !filter_var($email,FILTER_VALIDATE_EMAIL) ||
+     (!preg_match($mailregex,$email))) {
+      setcookie('email_error', '1', time() + 24*60 * 60);
+      setcookie('email_value', '', 100000);
+      $errors = TRUE;
+    }
+    else {
+      setcookie('email_value', $email, time() + 60 * 60);
+      setcookie('email_error','',100000);
+    }
+
+    if ($year=='Год') {
+      setcookie('year_error', '1', time() + 24 * 60 * 60);
+      setcookie('year_value', '', 100000);
+      $errors = TRUE;
+    }
+    else {
+      setcookie('year_value', intval($year), time() + 60 * 60);
+      setcookie('year_error','',100000);
+    }
+
+    if (!isset($pol)) {
+      setcookie('pol_error', '1', time() + 24 * 60 * 60);
+      setcookie('pol_value', '', 100000);
+      $errors = TRUE;
+    }
+    else {
+      setcookie('pol_value', $pol, time() + 60 * 60);
+      setcookie('pol_error','',100000);
+    }
+
+    if (!isset($limbs)) {
+      setcookie('limb_error', '1', time() + 24 * 60 * 60);
+      setcookie('limb_value', '', 100000);
+      $errors = TRUE;
+    }
+    else {
+      setcookie('limb_value', $limbs, time() + 60 * 60);
+      setcookie('limb_error','',100000);
+    }
+
+    if (!isset($powers)) {
+      setcookie('super_error', '1', time() + 24 * 60 * 60);
+      setcookie('immortal_value', '', 100000);
+      setcookie('noclip_value', '', 100000);
+      setcookie('power_value', '', 100000);
+      setcookie('telepat_value', '', 100000);
+      $errors = TRUE;
+    }
+    else {
+      $apw=array(
+        "immortal_value"=>0,
+        "noclip_value"=>0,
+        "power_value"=>0,
+        "telepat_value"=>0
+      );
+    foreach($powers as $pwer){
+      if($pwer=='immortal'){setcookie('immortal_value', 1, time() + 12 * 30 * 24 * 60 * 60); $apw['immortal_value']=1;} 
+      if($pwer=='noclip'){setcookie('noclip_value', 1, time() + 12*30 * 24 * 60 * 60);$apw['noclip_value']=1;} 
+      if($pwer=='power'){setcookie('power_value', 1, time() + 12*30 * 24 * 60 * 60);$apw['power_value']=1;} 
+      if($pwer=='telepat'){setcookie('telepat_value', 1, time() + 12*30 * 24 * 60 * 60);$apw['telepat_value']=1;}
+      }
+    foreach($apw as $c=>$val){
+      if($val==0){
+        setcookie($c,'',100000);
+        }
+      }
+    }
+    
+    if ((empty($bio)) || (!preg_match($bioregex,$bio))) {
+      setcookie('bio_error', '1', time() + 24 * 60 * 60);
+      setcookie('bio_value', '', 100000);
+      $errors = TRUE;
+    }
+    else {
+      setcookie('bio_value', $bio, time() + 12 * 30 * 24 * 60 * 60);
+      setcookie('bio_error', '', 100000);
+    }
+    
+    if ($errors) {
+      setcookie('save','',100000);
+      header('Location: ind.php?edit_id='.$id);
+    }
+    else {
+      setcookie('name_error', '', 100000);
+      setcookie('email_error', '', 100000);
+      setcookie('year_error', '', 100000);
+      setcookie('pol_error', '', 100000);
+      setcookie('limb_error', '', 100000);
+      setcookie('super_error', '', 100000);
+      setcookie('bio_error', '', 100000);
+      setcookie('check_error', '', 100000);
+    }
+    
+    $user = 'u41028';
+    $pass = '2356452';
+    $db = new PDO('mysql:host=localhost;dbname=u41028', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+    if(!$errors){
+      $id=$_SESSION['uid'];
+      $upd=$db->prepare("UPDATE application SET name=:name, email=:email, year=:byear, pol=:pol, konech=:limbs, biogr=:bio WHERE id=:id");
+      $cols=array(
+        ':name'=>$name,
+        ':email'=>$email,
+        ':byear'=>$year,
+        ':pol'=>$pol,
+        ':limbs'=>$limbs,
+        ':bio'=>$bio
+      );
+      foreach($cols as $k=>&$v){
+        $upd->bindParam($k,$v);
+      }
+      $upd->bindParam(':id',$id);
+      $upd->execute();
+      $del=$db->prepare("DELETE FROM superp WHERE per_id=?");
+      $del->execute(array($id));
+      $upd1=$db->prepare("INSERT INTO superp SET name=:power,per_id=:id");
+      $upd1->bindParam(':id',$id);
+      foreach($powers as $pwr){
+        $upd1->bindParam(':power',$pwr);
+        $upd1->execute();
+      }
+    }
+    
+    if(!$errors){
+      setcookie('save', '1');
+    }
+    header('Location: ind.php?edit_id='.$id);
+  }
+  else {
+    $id=$_POST['dd'];
+    $user = 'u41028';
+    $pass = '2356452';
+    $db = new PDO('mysql:host=localhost;dbname=u41028', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+    try {
+      $del=$db->prepare("DELETE FROM superp WHERE per_id=?");
+      $del->execute(array($id));
+      $stmt = $db->prepare("DELETE FROM application WHERE id=?");
+      $stmt -> execute(array($id));
+    }
+    catch(PDOException $e){
+      print('Error : ' . $e->getMessage());
+    exit();
+    }
+    setcookie('del','1');
+    setcookie('del_user',$id);
+    header('Location: admin.php');
+  }
+
+}
